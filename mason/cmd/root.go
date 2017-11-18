ceb348c8b32193bbcb5cf4a0dee9f8a1c8f6f0c8
Nik Ogura <nik.ogura@gmail.com>
2017-11-17 20:11:05 -0800

    replaced boilerplate in cobra commands

--- mason/cmd/root.go
+++ mason/cmd/root.go
@@ -28,13 +28,11 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "mason",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Tool for building Go binaries in a clean GOPATH.",
+	Long: `
+Tool for building Go binaries in a clean GOPATH.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
