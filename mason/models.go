2aa659ce0f07af9f9a1bdf0998567448f07e545c
Nik Ogura <nik.ogura@gmail.com>
2017-11-17 20:00:53 -0800

    added models

---
+++ mason/models.go
@@ -0,0 +1,13 @@
+package mason
+
+type Metadata struct {
+	Path        string
+	Package     string
+	Version     string
+	WorkDir     string
+	ConfigDir   string
+	SigningKey  string
+	CodePath    string
+	GitPath     string
+	Description string
+}
