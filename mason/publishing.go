9f91ce452011c5ce06d6374bffebef2c52a0decd
Nik Ogura <nik.ogura@gmail.com>
2017-11-30 18:15:22 -0800

    added todo's

--- mason/publishing.go
+++ mason/publishing.go
@@ -1,1 +1,3 @@
 package mason
+
+// TODO  publish needs an option to create and upload the foo.md5, foo.sha1, foo.sha256 files if the repo doesn't do it automatically like artifactory does.
--- mason/signing.go
+++ mason/signing.go
@@ -1,1 +1,3 @@
 package mason
+
+// TODO choice of signing mechanisms builtin via go's openpgp, shell out to gpg, shell out to keybase?
