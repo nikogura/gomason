66f451af408e1f67f33cd1674ab7828377a78bb6
Nik Ogura <nik.ogura@gmail.com>
2017-12-04 15:16:32 -0800

    added a gpg priming command for ephemeral use

--- mason/signing_test.go
+++ mason/signing_test.go
@@ -75,8 +75,16 @@ Expire-Date: 0
 		log.Printf("Trustdb file: %s", trustdb)
 		log.Printf("Test key generation file: %s", keyFile)
 
+		// setup gpg
+		cmd := exec.Command(shellCmd, "--trustdb", trustdb, "--no-default-keyring", "--keyring", keyring, "--list-keys")
+		err = cmd.Run()
+		if err != nil {
+			log.Printf("****** Error priming gpg: %s *****", err)
+			t.Fail()
+		}
+
 		// generate a test key
-		cmd := exec.Command(shellCmd, "--trustdb", trustdb, "--no-default-keyring", "--keyring", keyring, "--batch", "--generate-key", keyFile)
+		cmd = exec.Command(shellCmd, "--trustdb", trustdb, "--no-default-keyring", "--keyring", keyring, "--batch", "--generate-key", keyFile)
 		err = cmd.Run()
 		if err != nil {
 			log.Printf("****** Error creating test key: %s *****", err)
