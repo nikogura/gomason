2e758d92f595387a7b8dba2a7c433103ff0767a5
Nik Ogura <nik.ogura@gmail.com>
2018-01-13 17:20:28 -0800

    re-enabled TestWholeShebang

--- mason/mason_test.go
+++ mason/mason_test.go
@@ -1,6 +1,7 @@
 package mason
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -37,29 +38,28 @@ func tearDown() {
 	}
 }
 
-// This test, while perhaps desirable from a coverage standpoint, adds hugely to the wallclock for the test suite, and is therefore disabled.  All the pieces of WholeShebang are tested separately.  It's really not necessary to do them again.
-//func TestWholeShebang(t *testing.T) {
-//	fileName := fmt.Sprintf("%s/%s", tmpDir, testMetadataFileName())
-//
-//	err := ioutil.WriteFile(fileName, []byte(testMetaDataJson()), 0644)
-//	if err != nil {
-//		log.Printf("Error writing metadata file: %s", err)
-//		t.Fail()
-//	}
-//
-//	wd, err := os.Getwd()
-//	if err != nil {
-//		log.Printf("Error determining working directory: %s", wd)
-//		t.Fail()
-//	}
-//
-//	os.Chdir(tmpDir)
-//
-//	err = WholeShebang(tmpDir, "master", true, true, false, true)
-//	if err != nil {
-//		log.Printf("Error running whole shebang: %s", err)
-//		t.Fail()
-//	}
-//
-//	os.Chdir(wd)
-//}
+func TestWholeShebang(t *testing.T) {
+	fileName := fmt.Sprintf("%s/%s", tmpDir, testMetadataFileName())
+
+	err := ioutil.WriteFile(fileName, []byte(testMetaDataJson()), 0644)
+	if err != nil {
+		log.Printf("Error writing metadata file: %s", err)
+		t.Fail()
+	}
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Printf("Error determining working directory: %s", wd)
+		t.Fail()
+	}
+
+	os.Chdir(tmpDir)
+
+	err = WholeShebang(tmpDir, "master", true, false, false, true)
+	if err != nil {
+		log.Printf("Error running whole shebang: %s", err)
+		t.Fail()
+	}
+
+	os.Chdir(wd)
+}
