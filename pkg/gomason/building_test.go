11d82e6b63cf9549ca3b8be6a7beae27de1c52d6
Nik Ogura <nik@scribd.com>
2018-06-23 09:38:44 -0700

    fixed test oversight exposed by test race condition- build without checkout and sync

--- pkg/gomason/building_test.go
+++ pkg/gomason/building_test.go
@@ -40,6 +40,36 @@ func TestBuild(t *testing.T) {
 	gomodule := "github.com/nikogura/gomason"
 	branch := "master"
 
+	err = GovendorInstall(gopath, true)
+	if err != nil {
+		log.Printf("Error installing Govendor: %s", err)
+		t.Fail()
+	}
+
+	if _, err := os.Stat(fmt.Sprintf("%s/go/bin/govendor", tmpDir)); os.IsNotExist(err) {
+		log.Printf("Govendor vailed to install.")
+		t.Fail()
+	}
+
+	log.Printf("Checking out Master Branch")
+
+	err = Checkout(gopath, testMetadataObj(), "master", true)
+	if err != nil {
+		log.Printf("Failed to checkout module: %s", err)
+		t.Fail()
+	}
+
+	if _, err := os.Stat(fmt.Sprintf("%s/src/%s/metadata.json", gopath, testModuleName())); os.IsNotExist(err) {
+		log.Printf("Failed to checkout module")
+		t.Fail()
+	}
+
+	err = GovendorSync(gopath, testMetadataObj(), true)
+	if err != nil {
+		log.Printf("Error runnig govendor sync: %s", err)
+		t.Fail()
+	}
+
 	err = Build(gopath, testMetadataObj(), branch, true)
 	if err != nil {
 		log.Printf("Error building: %s", err)
