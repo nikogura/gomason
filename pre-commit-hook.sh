bef4c0527caf1a6ca7c5f8418c944b51f8d42792
Nik Ogura <nik@scribd.com>
2018-11-09 12:01:18 -0800

    updated pre-commit hook to block commits where version in code does not match version in metadata

--- pre-commit-hook.sh
+++ pre-commit-hook.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 /usr/local/go/bin/gofmt -w ./
+
+METADATA_VERSION=$(grep version metadata.json | awk '{print $2}' | sed 's/[",]//g')
+
+CODE_VERSION=$(grep VERSION pkg/gomason/gomason.go | awk '{print$4}' | sed 's/"//g')
+
+if [[ ${METADATA_VERSION} != ${CODE_VERSION} ]]; then
+  echo "Versions do not match!"
+  echo "'VERSION' in pkg/gomason/gomason.go must match 'version' in metadata.json"
+  exit 1
+fi
