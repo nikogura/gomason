503e13e5381109364b1eed5397757d4d64596a02
Nik Ogura <nik@scribd.com>
2018-12-20 09:40:55 -0800

    added more tests

---
+++ pkg/gomason/testing_test.go
@@ -0,0 +1,34 @@
+package gomason
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestTest(t *testing.T) {
+	log.Printf("Checking out Master Branch")
+	gopath, err := CreateGoPath(tmpDir)
+	if err != nil {
+		log.Printf("Error creating GOPATH in %s: %s", tmpDir, err)
+		t.Fail()
+	}
+
+	err = Checkout(gopath, testMetadataObj(), "master", true)
+	if err != nil {
+		log.Printf("Failed to checkout module: %s", err)
+		t.Fail()
+	}
+
+	if _, err := os.Stat(fmt.Sprintf("%s/src/%s/metadata.json", gopath, testModuleName())); os.IsNotExist(err) {
+		log.Printf("Failed to checkout module")
+		t.Fail()
+	}
+
+	err = GoTest(gopath, testMetadataObj().Package, true)
+	if err != nil {
+		log.Printf("error running go test: %s", err)
+		t.Fail()
+	}
+}
